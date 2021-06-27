12b9d8ac01511bcd49337d4686075443128d078d
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-06-27 16:59:40 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
+ class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      displayName:'',
+           
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changeHandler=(e)=>{
+    this.setState({
+      displayName:e.target.value
+
+   })
+  }
+
+  submitData=async (e)=>{
+    e.preventDefault()
+    let name= await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.b25689e45ce9b253dc8d624cf71fa8e1&city=${this.state.displayName}&format=json`)
+    this.setState({
+      displayName:name.data[0].display_name,
+      longitude:name.data[0].lon,
+      latitude:name.data[0].lat
+    })
+    console.log(name.data[0].lat)
+  }
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitData}>
+          <input type='text' placeholder='city name....' onChange={(e)=>{this.changeHandler(e)}} />
+          <button>Explore</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
